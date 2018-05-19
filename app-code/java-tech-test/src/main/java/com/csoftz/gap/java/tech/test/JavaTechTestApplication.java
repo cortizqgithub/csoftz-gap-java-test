832f40737573059a4a8adcd575a21e6295e6e550
Carlos Adolfo Ortiz Quiros (Mac) <carlosaortiz@yahoo.com>
2018-05-19 07:56:10 -0500

    Creates the first initial commit by providing starting files for documentation and app code.

---
+++ app-code/java-tech-test/src/main/java/com/csoftz/gap/java/tech/test/JavaTechTestApplication.java
@@ -0,0 +1,12 @@
+package com.csoftz.gap.java.tech.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTechTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTechTestApplication.class, args);
+    }
+}
---
+++ app-code/java-tech-test/src/test/java/com/csoftz/gap/java/tech/test/JavaTechTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.csoftz.gap.java.tech.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaTechTestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
